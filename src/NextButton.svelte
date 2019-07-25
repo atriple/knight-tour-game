13ab69012f9b3efc5041ff7bfc02e0985f5b2d88
atriple <andiaqil.dpe@gmail.com>
2019-07-25 13:35:55 +0700

    Add Next Button

---
+++ src/NextButton.svelte
@@ -0,0 +1,17 @@
+<script>
+    import { choosed_count, current_level, win_condition, current_index, level_list } from './stores.js';
+
+    let index;
+
+    function nextLevel(){
+        current_index.update(n => n + 1);
+        current_index.subscribe(val => index = val);
+        level_list.subscribe(val => current_level.set(val[index]));
+        current_level.subscribe(val => win_condition.set((val.size ** 2) - val.block.length));
+        choosed_count.set(20);}
+
+</script>
+
+<button on:click={nextLevel}>
+Next Level
+</button>
