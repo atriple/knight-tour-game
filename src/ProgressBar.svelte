97a719c9ff612fb5c73291f26191f563a75a71eb
atriple <andiaqil.dpe@gmail.com>
2019-08-15 14:10:58 +0700

    Add progress bar style

--- src/ProgressBar.svelte
+++ src/ProgressBar.svelte
@@ -1,3 +1,44 @@
 <script>
+	import { tweened } from 'svelte/motion';
+    import { cubicOut } from 'svelte/easing';
+    import { choosed_count, win_condition} from './stores.js';
 
+	const progress = tweened(0, {
+		duration: 400,
+		easing: cubicOut
+    });
+    
+    choosed_count.subscribe(val => {
+        progress.set(val / ($win_condition + 1));
+    });
+    
 </script>
+
+<style>
+    .bar{
+        margin: 10px;
+    }
+
+    progress[value]{
+        border-radius: 10px;
+        
+    }
+
+	progress[value]::-webkit-progress-bar {
+        background-color: #F1F2EF;
+        border-radius: 10px;
+        box-shadow: 0 2px 5px rgba(0, 0, 0, 0.25) inset;
+    }
+
+    progress[value]::-webkit-progress-value{
+        background-color: goldenrod;
+        border-radius: 10px;
+    }
+
+    
+</style>
+
+<div class="bar">
+    <progress value={$progress}></progress>
+    {$choosed_count} / {$win_condition}(+1)
+</div>
