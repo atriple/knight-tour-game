425807e55fdbfa191773d532acb9e4fad4793a37
atriple <andiaqil.dpe@gmail.com>
2019-08-20 14:23:20 +0700

    Refactor reset button

--- src/Reset.svelte
+++ src/Reset.svelte
@@ -1,12 +1,13 @@
 <script>
-    import { deepCopy, current_level, active_grid, initiated, choosed_count } from './stores.js';
+    import { current_level, active_grid, initiated, choosed_count } from './stores.js';
+    import { deepCopy } from './utils.js';
 
     function reset(){
-        console.log("Reset button pressed");
+        //console.log("Reset button pressed");
         $active_grid = deepCopy($current_level.grid);
         initiated.set(false);
         choosed_count.set(0);
-        }
+    }
 
 </script>
 
