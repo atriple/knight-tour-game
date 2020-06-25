6ad0e55bb00e5547f115cd969ffc167f955589d6
Andi Aqil A <40452321+atriple@users.noreply.github.com>
2020-06-25 15:45:51 +0700

    Update start menu

--- src/App.svelte
+++ src/App.svelte
@@ -21,10 +21,8 @@
 </script>
 
 <style>
-  .header-gap {
-    order: 3;
-    width: 25%;
-    margin-right: 0px;
+  .button {
+    @apply bg-purple-300 rounded-md w-32 h-12 text-center text-xl shadow-md my-12 mx-4;
   }
 
   .game {
@@ -35,15 +33,23 @@
 </style>
 
 {#if menuState == 'begin'}
-  <h1>Knight Adventure</h1>
-  <button on:click={playGame}>New Game</button>
-  <button>Continue</button>
+  <div class="w-1/2 h-screen text-center m-auto py-20">
+    <h1 class="font-bold">Knight Adventure</h1>
+    <button
+      on:click={playGame}
+      class="button hover:bg-purple-600 hover:text-white">
+      New Game
+    </button>
+    <button class="button hover:bg-purple-600 hover:text-white">
+      Continue
+    </button>
+  </div>
 {/if}
 
 {#if menuState == 'game'}
-  <header class="flex flex-row justify-between">
+  <header class="flex justify-between">
     <Reset />
-    <h2 class="level">Knight Adventure</h2>
+    <h2 class="w-1/2 m-auto text-center">Knight Adventure</h2>
     <p>How to play?</p>
   </header>
 
@@ -61,5 +67,3 @@
     </div>
   {/if}
 </div>
-
-<footer>Created by atriple with QM team.</footer>
