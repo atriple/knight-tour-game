898dd65b0378c5f715d518b55a58a73d5ac35c47
Andi Aqil A <40452321+atriple@users.noreply.github.com>
2020-11-09 00:49:18 +0700

    Reboot version 2.1

--- src/App.svelte
+++ src/App.svelte
@@ -1,69 +1,34 @@
 <script>
-  import Tailwindcss from "./Tailwindcss.svelte";
-  import {
-    choosed_count,
-    current_level,
-    win_condition,
-    current_index,
-    level_list,
-    loadLastSession,
-    resetSession
-  } from "./stores.js";
-  import Board from "./Board.svelte";
-  import Reset from "./Reset.svelte";
-  import ProgressBar from "./ProgressBar.svelte";
-
-  let menuState = "begin";
-
-  const playGame = () => {
-    menuState = "game";
-  };
+  export let name;
 </script>
 
 <style>
-  .button {
-    @apply bg-purple-300 rounded-md w-32 h-12 text-center text-xl shadow-md my-12 mx-4;
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
   }
 
-  .game {
-    margin: 50px auto;
-    text-align: center;
-    width: 50%;
+  h1 {
+    color: #ff4000;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
   }
 </style>
 
-{#if menuState == 'begin'}
-  <div class="w-1/2 h-screen text-center m-auto py-20">
-    <h1 class="font-bold">Knight Adventure</h1>
-    <button
-      on:click={playGame}
-      class="button hover:bg-purple-600 hover:text-white">
-      New Game
-    </button>
-    <button class="button hover:bg-purple-600 hover:text-white">
-      Continue
-    </button>
-  </div>
-{/if}
-
-{#if menuState == 'game'}
-  <header class="flex justify-between">
-    <Reset />
-    <h2 class="w-1/2 m-auto text-center">Knight Adventure</h2>
-    <p>How to play?</p>
-  </header>
-
-  <div class="game">
-    <Board />
-    <ProgressBar />
-  </div>
-{/if}
-
-<div>
-
-  {#if $choosed_count == $win_condition + 1}
-    <div align="center">
-      <h1>YOU WIN! 🏆</h1>
-    </div>
-  {/if}
-</div>
+<main>
+  <h1>Hello {name}!</h1>
+  <p>
+    Visit the
+    <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
+    to learn how to build Svelte apps.
+  </p>
+</main>
