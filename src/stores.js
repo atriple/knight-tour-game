d31f1defded668cdfb3d2d340d86a55a05720e94
Andi Aqil A <40452321+atriple@users.noreply.github.com>
2020-11-27 18:59:32 +0700

    Add stores

---
+++ src/stores.js
@@ -0,0 +1,16 @@
+import { getTotalLevel } from "./data_config";
+import { writable } from "svelte/store";
+
+const STATE = {
+  //Trivia: Each individual object assigned will be unique, so it's good idea to assign value to {}
+  BEGIN: {},
+  GAME: {},
+  CHOOSE_LEVEL: {},
+  HOW_TO_PLAY: {},
+};
+
+const currentState = writable(STATE.BEGIN);
+const currentLevel = writable(0);
+const finishedLevel = writable(getTotalLevel());
+
+export { STATE, currentState, currentLevel, finishedLevel };
