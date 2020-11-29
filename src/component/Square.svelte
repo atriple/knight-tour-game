552a8f4a5777e2099a381478c5040f23219e9d59
Andi Aqil A <40452321+atriple@users.noreply.github.com>
2020-11-29 14:23:25 +0700

    <Square> : add knight state & event dispatcher

--- src/component/Square.svelte
+++ src/component/Square.svelte
@@ -10,15 +10,23 @@
 
   $: isDark = (row + col) % 2 === 0;
   $: blocked = value === GRID_STATE.BLOCKED;
+  $: knight = value === GRID_STATE.CURRENT;
 
   const dispatch = createEventDispatcher();
+
+  function sendPosition() {
+    dispatch("position", {
+      row: row,
+      col: col,
+    });
+  }
 </script>
 
 <style>
   div {
     padding-top: 100%;
     position: relative;
-    border: 1px solid black;
+    border: 2px solid rgb(73, 73, 73);
     margin: 1px;
   }
 
@@ -36,6 +44,17 @@
     background-position: center;
     background-size: 54px;
   }
+
+  .knight {
+    background-image: url("/img/knight-black-hi.png");
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: 54px;
+  }
 </style>
 
-<div class={isDark ? 'dark' : 'light'} class:blocked />
+<div
+  class={isDark ? 'dark' : 'light'}
+  class:blocked
+  class:knight
+  on:click={sendPosition} />
