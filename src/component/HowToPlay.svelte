2bf5c502c0432f92b06000ebd70e3a5f7dd444e2
Andi Aqil A <40452321+atriple@users.noreply.github.com>
2020-11-29 18:08:45 +0700

    Implement <HowToPlay>

---
+++ src/component/HowToPlay.svelte
@@ -0,0 +1,27 @@
+<script>
+  import ButtonToMenu from "./button/ButtonToMenu.svelte";
+</script>
+
+<style>
+  p {
+    margin: auto;
+  }
+
+  div {
+    margin-top: 24px;
+  }
+</style>
+
+<p>Conquer the land for the glory of our king!<br /></p>
+<p>
+  <br />The game has similar mechanism to
+  <a href="https://en.wikipedia.org/wiki/Knight%27s_tour">Knight Tour Problems</a>.
+  <br />
+  You have a piece of knight chess where you need to visit every empty block
+  once.
+  <br />
+</p>
+<p>You can start your campaign at any position you like!</p>
+<div>
+  <ButtonToMenu />
+</div>
